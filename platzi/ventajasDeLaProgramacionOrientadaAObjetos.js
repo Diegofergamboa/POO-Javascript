8e4cb5bd01b496453fb110b49a54751062823510
Diegofergamboa <diegofergamboa@hotmail.es>
2021-12-11 16:48:06 -0500

    Aprendiendo como realizar cambios con instancias y clases no objetos literales.

--- platzi/ventajasDeLaProgramacionOrientadaAObjetos.js
+++ platzi/ventajasDeLaProgramacionOrientadaAObjetos.js
@@ -10,16 +10,16 @@
 //         'Cursos definitivo de CSS' ,
 //         'Mateámtica financiera'
 //     ] ,
-//     learningPaths: [
-//         {
-//             name : 'Escuela de Desarollo Web' ,
-//             courses : ['Responsive design' , 'HTML' , 'CSS']
-//         } ,
-//         {
-//             name : 'Escuela de DataScience' ,
-//             courses : ['Básico de Python' , 'POO con Python' , 'Rankers']
-//         } ,
-//     ]
+    // learningPaths: [
+    //     {
+    //         name : 'Escuela de Desarollo Web' ,
+    //         courses : ['Responsive design' , 'HTML' , 'CSS']
+    //     } ,
+    //     {
+    //         name : 'Escuela de DataScience' ,
+    //         courses : ['Básico de Python' , 'POO con Python' , 'Rankers']
+    //     } ,
+    // ]
 // }
 // const miguel = {
 //     name : "Jmiguel" ,
@@ -44,3 +44,54 @@
 //         } ,
 //     ]
 // }
+
+
+//! Construcción del objeto Student.
+
+
+class Student {
+    constructor({
+        name ,
+        username ,
+        point ,
+        socialMedia = {
+            twitter = undefined ,
+            facebook = undefined
+        } ,
+        aprovesCourses = [] ,
+        learningPaths = [] ,
+    }) {
+        this.name = name,
+        this.username = username,
+        this.point = point ,
+        this.socialMedia = {
+            twitter ,
+            facebook
+        } ,
+        this.aprovesCourses = aprovesCourses ,
+        this.learningPaths = learningPaths 
+    }
+}
+
+//! Construyendolos por instancias.
+
+const diego = new Student({
+    name : 'Diego' ,
+    username : 'diegofergamboa' ,
+    point : 10000 ,
+    socialMedia : {
+        twitter : 'diegogamboaor' ,
+        facebook : '¿Quién usa Facebook? xD'
+    } ,
+    aprovesCourses : ['Cursos definitivo de CSS' , 'Mateámtica financiera'] ,
+    learningPaths: [
+        {
+            name : 'Escuela de Desarollo Web' ,
+            courses : ['Responsive design' , 'HTML' , 'CSS']
+        } ,
+        {
+            name : 'Escuela de DataScience' ,
+            courses : ['Básico de Python' , 'POO con Python' , 'Rankers']
+        } ,
+    ]
+})
