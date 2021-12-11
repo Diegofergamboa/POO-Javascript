3063aa856f606fd414386dce65fefff422cccabd
Diegofergamboa <diegofergamboa@hotmail.es>
2021-12-11 14:10:16 -0500

    Update objetosLiteralesYPrototiposenJS.js

---
+++ platzi/objetosLiteralesYPrototiposenJS.js
@@ -0,0 +1,12 @@
+const natalia = {
+    name: "Natalia" ,
+    age: 20 ,
+    cursos: [
+        'Inglés' ,
+        'HTML y CSS'
+    ]
+};
+console.log(natalia);
+natalia.cursos.push('Matemáticas');
+
+natalia.name = 'Nath';
