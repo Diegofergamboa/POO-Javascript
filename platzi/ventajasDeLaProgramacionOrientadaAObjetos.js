823fddfb956e3ff18b60dee24f5d14d0c94adfa1
Diegofergamboa <diegofergamboa@hotmail.es>
2021-12-11 16:14:19 -0500

    Creación de objetos literales.

---
+++ platzi/ventajasDeLaProgramacionOrientadaAObjetos.js
@@ -0,0 +1,46 @@
+const juan = {
+    name : "JuanDC" ,
+    username : 'juanDC' ,
+    points : 100 ,
+    socialMedia : {
+        twitter : 'juanDC' ,
+        facebook : 'facebook.com/JC'
+    } ,
+    aprovedCourses : [
+        'Cursos definitivo de CSS' ,
+        'Mateámtica financiera'
+    ] ,
+    learningPaths: [
+        {
+            name : 'Escuela de Desarollo Web' ,
+            courses : ['Responsive design' , 'HTML' , 'CSS']
+        } ,
+        {
+            name : 'Escuela de DataScience' ,
+            courses : ['Básico de Python' , 'POO con Python' , 'Rankers']
+        } ,
+    ]
+}
+const miguel = {
+    name : "Jmiguel" ,
+    username : 'miguelin' ,
+    points : 1000 ,
+    socialMedia : {
+        twitter : 'miguelDC' ,
+        facebook : null
+    } ,
+    aprovedCourses : [
+        'Cursos definitivo de CSS' ,
+        'Finanzas'
+    ] ,
+    learningPaths: [
+        {
+            name : 'ANCD' ,
+            courses : ['Responsive Design' , 'HTML' , 'CSS']
+        } ,
+        {
+            name : 'Escuela de VideoJuegos' ,
+            courses : ['Tabloi' , 'POO básico' , 'Uniti']
+        } ,
+    ]
+}
