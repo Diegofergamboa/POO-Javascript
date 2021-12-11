3d06b841b2e880d288086d7d0b33485ac5ca91fd
Diegofergamboa <diegofergamboa@hotmail.es>
2021-12-11 15:55:16 -0500

    atributos de clase de tipo objeto.

--- platzi/clasesEnJavaScript.js
+++ platzi/clasesEnJavaScript.js
@@ -1,3 +1,25 @@
 
 //! Prototipos en la sintaxis de clases.
 
+class Student {
+    //! Al colocar dentro de los atributos de nuestra clase, un objeto. Nos permitirá realizar construcciones de atributos de instancias que no existen. Por ejemplo
+    constructor({
+        name , 
+        age , 
+        cursos = []
+    }) 
+    {
+        this.name = name ;
+        this.age = age ;
+        this.cursos = cursos ;
+    }
+
+    aprobarCursos(nuevoCurso) {
+        this.cursos.push(nuevoCurso);
+    }
+}
+
+// Elemento instancia de un prototipo.
+const miguel = new Student('Miguel', 26, ['React Hooks', 'Node.JS']);
+
+//! Al colocar dentro de los atributos de nuestra clase, un objeto. Nos permitirá realizar construcciones de atributos de instancias que no existen. Por ejemplo
