f596aa58e6beb3d1a98855a9175f748daa2aa42a
Diegofergamboa <diegofergamboa@hotmail.es>
2021-12-12 16:44:49 -0500

    aa

--- module.mjs
+++ module.mjs
@@ -4,3 +4,10 @@ const clase67 = new PlatziClass({
     nombre : "JavScript Orientado a Objetos" ,
     videoID : "215"
 })
+
+clase67.reproducir();
+clase67.pausar();
+clase67.reproducir();
+clase67.pausar();
+clase67.reproducir();
+clase67.pausar();
